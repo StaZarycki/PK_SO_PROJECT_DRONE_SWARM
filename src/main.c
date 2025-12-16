6b12dc5908247ebb20b435999b1cf264d24835e0
StaZarycki <stazarycki@gmail.com>
2025-12-16 12:01:33 +0100

    feat: check and parse drone amount

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
-#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-  print_hello();
+  int drone_amount = 0;
+
+  if (argc >= 2) {
+    char *response;
+    drone_amount = strtol(argv[1], &response, 10);
+
+    if (strcmp(response, ""))
+      printf("Could not parse %s to int.\n", response);
+
+    return 1;
+  }
+
+  printf("Drone amount: %d\n", drone_amount);
 
   return 0;
 }
